612feb52b90bf1949ece40dc4f5283e3b1e8089e
ruh0n <wns8256@naver.com>
2024-02-23 20:58:50 +0900

    🏷️ fix(auth): implement 'ROLE_' in a user role enum for Spring Security

--- src/main/java/com/nbcampif/ifstagram/domain/user/UserRole.java
+++ src/main/java/com/nbcampif/ifstagram/domain/user/UserRole.java
@@ -1,6 +1,13 @@
 package com.nbcampif.ifstagram.domain.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
-  ADMIN,
-  USER,
+  ADMIN("ROLE_ADMIN"),
+  USER("ROLE_USER"),
+  ;
+  private final String role;
 }
