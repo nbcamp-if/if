aefe3e1a25cc04265ecb9b447fea38a076f340b4
namji <skawlgns07@naver.com>
2024-02-23 15:13:38 +0900

    first commit

---
+++ src/main/java/com/nbcampif/ifstagram/IfstagramApplication.java
@@ -0,0 +1,13 @@
+package com.nbcampif.ifstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfstagramApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IfstagramApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nbcampif/ifstagram/IfstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbcampif.ifstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfstagramApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
