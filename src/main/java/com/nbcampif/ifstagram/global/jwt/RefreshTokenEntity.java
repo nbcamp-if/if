cc7d532e48f32f317dac42cceb974b5d180ab861
ruh0n <wns8256@naver.com>
2024-02-23 20:05:41 +0900

    ✨ feat(auth): implement repository and entity for storing refresh tokens

---
+++ src/main/java/com/nbcampif/ifstagram/global/jwt/RefreshTokenEntity.java
@@ -0,0 +1,34 @@
+package com.nbcampif.ifstagram.global.jwt;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "refresh_tokens")
+@Entity
+public class RefreshTokenEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "refresh_token_id")
+  private Long id;
+  private Long userId;
+  private String token;
+
+  private RefreshTokenEntity(Long userId, String token) {
+    this.userId = userId;
+    this.token = token;
+  }
+
+  public static RefreshTokenEntity of(Long memberId, String token) {
+    return new RefreshTokenEntity(memberId, token);
+  }
+
+}
