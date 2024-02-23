d5bc5f7593fd59fbff9d685b84a34683600fc7f2
ruh0n <wns8256@naver.com>
2024-02-23 15:56:38 +0900

    :sparkles: feat: implement properties to application

--- src/main/java/com/nbcampif/ifstagram/IfstagramApplication.java
+++ src/main/java/com/nbcampif/ifstagram/IfstagramApplication.java
@@ -2,8 +2,10 @@ package com.nbcampif.ifstagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:db.properties", "classpath:jwt.properties"})
 public class IfstagramApplication {
 
   public static void main(String[] args) {
