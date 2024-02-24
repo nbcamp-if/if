77772c254de950695eaf814e275da59a32efd95d
ruh0n <wns8256@naver.com>
2024-02-24 22:04:03 +0900

    ✨ feat(user): implement repository and entity for storing user

---
+++ src/main/java/com/nbcampif/ifstagram/domain/user/model/User.java
@@ -0,0 +1,63 @@
+package com.nbcampif.ifstagram.domain.user.model;
+
+import com.nbcampif.ifstagram.domain.user.UserRole;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@Getter
+@AllArgsConstructor
+public class User implements OAuth2User {
+
+  Long userId;
+  String email;
+  String nickname;
+  String profileImage;
+  String introduction;
+  Long reportedCount;
+  UserRole role;
+
+  public User(Long userId, String email, String nickname, String profileImage) {
+    this.userId = userId;
+    this.email = email;
+    this.nickname = nickname;
+    this.profileImage = profileImage;
+    this.role = UserRole.USER;
+  }
+
+  public User(Long userId, String email, String nickname, String profileImage, UserRole role) {
+    this.userId = userId;
+    this.email = email;
+    this.nickname = nickname;
+    this.profileImage = profileImage;
+    this.role = role;
+  }
+
+  public String getRole() {
+    return role.getRole();
+  }
+
+  public UserRole getUserRole() {
+    return role;
+  }
+
+  @Override
+  public Map<String, Object> getAttributes() {
+    return Collections.emptyMap();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public String getName() {
+    return this.nickname;
+  }
+
+}
---
+++ src/main/java/com/nbcampif/ifstagram/domain/user/repository/entity/UserEntity.java
@@ -0,0 +1,65 @@
+package com.nbcampif.ifstagram.domain.user.repository.entity;
+
+import com.nbcampif.ifstagram.domain.common.entity.TimestampEntity;
+import com.nbcampif.ifstagram.domain.user.UserRole;
+import com.nbcampif.ifstagram.domain.user.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@SQLRestriction(value = "deleted_at is NULL")
+@SQLDelete(sql = "UPDATE users SET deleted_at = NOW() WHERE user_id = ?")
+public class UserEntity extends TimestampEntity {
+
+  @Id
+  private Long userId;
+
+  @Column
+  private String email;
+
+  @Column(nullable = false)
+  private String nickname;
+
+  @Column
+  private String profileImage;
+
+  @Column
+  private String introduction;
+
+  @Column
+  private Long reportedCount;
+
+  @Column
+  @Enumerated(value = EnumType.STRING)
+  private UserRole role;
+
+  public static UserEntity fromModel(User user) {
+    return new UserEntity(
+        user.getUserId(),
+        user.getEmail(),
+        user.getNickname(),
+        user.getProfileImage(),
+        user.getIntroduction(),
+        user.getReportedCount(),
+        user.getUserRole()
+    );
+  }
+
+  public User toModel() {
+    return new User(userId, email, nickname, profileImage, introduction, reportedCount, role);
+  }
+
+}
