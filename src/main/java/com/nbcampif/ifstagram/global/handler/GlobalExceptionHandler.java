dbda955895aa2c1d4dbbf307b25318419072be6e
ruh0n <wns8256@naver.com>
2024-02-24 22:07:41 +0900

    ✨ feat(global): implement GlobalExceptionHandler

---
+++ src/main/java/com/nbcampif/ifstagram/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.nbcampif.ifstagram.global.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
