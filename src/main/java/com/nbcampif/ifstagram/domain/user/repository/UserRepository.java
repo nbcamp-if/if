6310b061b886acd1a9001ef6e39187fc15910713
ruh0n <wns8256@naver.com>
2024-02-24 22:04:03 +0900

    ✨ feat(user): implement repository and entity for storing user

---
+++ src/main/java/com/nbcampif/ifstagram/domain/user/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package com.nbcampif.ifstagram.domain.user.repository;
+
+import com.nbcampif.ifstagram.domain.user.model.User;
+import com.nbcampif.ifstagram.domain.user.repository.entity.UserEntity;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class UserRepository {
+
+  private final UserJpaRepository userJpaRepository;
+
+  public void createUser(User user) {
+    userJpaRepository.save(UserEntity.fromModel(user));
+  }
+
+  public Optional<User> findUser(Long userId) {
+    return userJpaRepository.findById(userId).map(UserEntity::toModel);
+  }
+
+  public User findUserOrElseThrow(Long userId) {
+    return findUser(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
+  }
+
+}
