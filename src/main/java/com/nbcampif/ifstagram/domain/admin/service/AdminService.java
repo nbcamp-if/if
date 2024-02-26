88c53e267c0c9dbd4253c4bac8859c6022667e4d
Hu6r1s <darmaa333@gmail.com>
2024-02-26 19:06:30 +0900

    :sparkles: feat(admin): admin login function

--- src/main/java/com/nbcampif/ifstagram/domain/admin/service/AdminService.java
+++ src/main/java/com/nbcampif/ifstagram/domain/admin/service/AdminService.java
@@ -1,11 +1,15 @@
 package com.nbcampif.ifstagram.domain.admin.service;
 
+import com.nbcampif.ifstagram.domain.admin.dto.LoginRequestDto;
 import com.nbcampif.ifstagram.domain.user.UserRole;
 import com.nbcampif.ifstagram.domain.user.model.User;
 import com.nbcampif.ifstagram.domain.user.repository.UserRepository;
+import com.nbcampif.ifstagram.global.jwt.JwtTokenProvider;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +19,10 @@ import org.springframework.stereotype.Service;
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Value("${admin.password}")
+    private String verifyPassword;
 
     @PostConstruct
     public void createAdminAccount() {
@@ -28,6 +36,15 @@ public class AdminService {
                 UserRole.ADMIN
             );
             userRepository.createUser(admin);
+        }
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse response) {
+        String password = requestDto.getPassword();
+        User user = userRepository.findByEmail(requestDto.getEmail());
+        if (verifyPassword.equals(password) && user.getRole().equals(UserRole.ADMIN)) {
+            String accessToken = jwtTokenProvider.generateAccessToken(user.getUserId(), user.getRole().getAuthority());
+            jwtTokenProvider.addAccessTokenToCookie(accessToken, response);
         }
     }
 }
