f3ac82475051e2130add2f0d8e410fad71b505dc
ruh0n <wns8256@naver.com>
2024-02-27 09:09:30 +0900

    ✨ feat(user): implement controller and service for user

--- src/main/java/com/nbcampif/ifstagram/domain/user/controller/UserController.java
+++ src/main/java/com/nbcampif/ifstagram/domain/user/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.nbcampif.ifstagram.domain.user.controller;
 
-import com.nbcampif.ifstagram.global.dto.CommonResponse;
+import com.nbcampif.ifstagram.domain.user.dto.UserResponseDto;
+import com.nbcampif.ifstagram.domain.user.dto.UserUpdateRequestDto;
+import com.nbcampif.ifstagram.domain.user.model.User;
 import com.nbcampif.ifstagram.domain.user.service.UserService;
+import com.nbcampif.ifstagram.global.dto.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/reports/users")
@@ -25,4 +31,28 @@ public class UserController {
         System.out.println(userId);
         return userService.reportUser(userId);
     }
+
+  @GetMapping("/my-page")
+  public ResponseEntity<UserResponseDto> myPage(@AuthenticationPrincipal User user) {
+    return ResponseEntity.ok(UserResponseDto.of(user));
+  }
+
+  @PostMapping("/{userId}/follow")
+  public ResponseEntity<Void> follow(
+      @PathVariable(name = "userId") Long toUserId,
+      @AuthenticationPrincipal User user
+  ) {
+    userService.follow(user, toUserId);
+    return ResponseEntity.ok().build();
+  }
+
+  @PutMapping("/my-page")
+  public ResponseEntity<UserResponseDto> updateUser(
+      @Validated UserUpdateRequestDto requestDto,
+      @AuthenticationPrincipal User user
+  ) {
+    User updatedUser = userService.updateUser(requestDto, user);
+    return ResponseEntity.ok(UserResponseDto.of(updatedUser));
+  }
+
 }
