59d8ddd9efd762198e2e0ef03f913bb60fb66f5a
ruh0n <wns8256@naver.com>
2024-02-27 22:29:26 +0900

    ✨ feat(user): implement logout method

--- src/main/java/com/nbcampif/ifstagram/global/config/WebSecurityConfig.java
+++ src/main/java/com/nbcampif/ifstagram/global/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.nbcampif.ifstagram.global.config;
 
 import com.nbcampif.ifstagram.domain.user.UserRole;
 import com.nbcampif.ifstagram.global.filter.JwtAuthenticationFilter;
+import com.nbcampif.ifstagram.global.handler.CustomLogoutHandler;
 import com.nbcampif.ifstagram.global.handler.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -22,37 +23,41 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+  private static final String AUTHORIZATION_ACCESS_TOKEN_HEADER_KEY = "Authorization";
+
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
   private final DefaultOAuth2UserService authService;
   private final OAuth2SuccessHandler oAuth2SuccessHandler;
+  private final CustomLogoutHandler customLogoutHandler;
+
 
   @Bean
   protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
     // disable csrf
     http.csrf(AbstractHttpConfigurer::disable);
     // disable session
-    http.sessionManagement(
-        management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     // config jwt filter
-    http.authorizeHttpRequests(request ->
-        request.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-            .permitAll() // resources 접근 허용 설정
-            .requestMatchers("/", "/api/v1/auth/**", "/oauth2/**", "/api/v1/admin/login")
-            .permitAll() // 인증 관련
-            .requestMatchers("/api/v1/admin/**")
-            .hasRole(UserRole
-            .ADMIN.name())
-            .requestMatchers("/v3/**", "/swagger-ui/**")
-            .permitAll() // swagger
-            .anyRequest()
-            .authenticated()
-    );
+    http.authorizeHttpRequests(request -> request.requestMatchers(PathRequest.toStaticResources()
+            .atCommonLocations())
+        .permitAll() // resources 접근 허용 설정
+        .requestMatchers("/", "/api/v1/auth/**", "/oauth2/**", "/api/v1/admin/login")
+        .permitAll() // 인증 관련
+        .requestMatchers("/api/v1/admin/**")
+        .hasRole(UserRole.ADMIN.name())
+        .requestMatchers("/v3/**", "/swagger-ui/**")
+        .permitAll() // swagger
+        .anyRequest()
+        .authenticated());
     // config oauth2 filter
     http.oauth2Login(oauth2 -> oauth2.authorizationEndpoint(authorization -> authorization.baseUri("/api/v1/auth/login"))
-        .redirectionEndpoint(
-                redirection -> redirection.baseUri("/api/v1/auth/login/oauth2/callback/*"))
+        .redirectionEndpoint(redirection -> redirection.baseUri("/api/v1/auth/login/oauth2/callback/*"))
         .userInfoEndpoint(userInfo -> userInfo.userService(authService))
         .successHandler(oAuth2SuccessHandler));
+    // config logout
+    http.logout(logout -> logout.logoutUrl("/api/v1/auth/logout")
+        .addLogoutHandler(customLogoutHandler)
+        .deleteCookies(AUTHORIZATION_ACCESS_TOKEN_HEADER_KEY));
     // add jwt filter
     http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
