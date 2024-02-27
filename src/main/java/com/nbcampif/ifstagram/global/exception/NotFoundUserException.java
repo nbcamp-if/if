e96ba3fab2232d249723bc828a0fbb7e87eb418d
Hu6r1s <darmaa333@gmail.com>
2024-02-27 10:26:46 +0900

    :sparkles: feat(exception): admin login exception

---
+++ src/main/java/com/nbcampif/ifstagram/global/exception/NotFoundUserException.java
@@ -0,0 +1,9 @@
+package com.nbcampif.ifstagram.global.exception;
+
+public class NotFoundUserException extends RuntimeException {
+
+
+    public NotFoundUserException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/nbcampif/ifstagram/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/nbcampif/ifstagram/global/handler/GlobalExceptionHandler.java
@@ -1,8 +1,25 @@
 package com.nbcampif.ifstagram.global.handler;
 
+import com.nbcampif.ifstagram.global.exception.NotFoundUserException;
+import com.nbcampif.ifstagram.global.exception.PermissionNotException;
+import com.nbcampif.ifstagram.global.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(NotFoundUserException.class)
+    public ResponseEntity<ErrorResponse> handleNotFountUserException(NotFoundUserException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(errorResponse);
+    }
+
+    @ExceptionHandler(PermissionNotException.class)
+    public ResponseEntity<ErrorResponse> handlePermissionNotException(PermissionNotException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(errorResponse);
+    }
 }
