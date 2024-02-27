eb3ac7095c8899a9f11c4db112611897c6aa3dcc
ruh0n <wns8256@naver.com>
2024-02-27 15:26:36 +0900

    ✨ feat(global): implement constructor method for OK response in CommonResponse

--- src/main/java/com/nbcampif/ifstagram/global/dto/CommonResponse.java
+++ src/main/java/com/nbcampif/ifstagram/global/dto/CommonResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @Setter
@@ -14,5 +16,14 @@ public class CommonResponse<T> {
   private Integer statusCode;
   private String msg;
   private T data;
+
+  public static <T> ResponseEntity<CommonResponse<T>> ok(String msg, T data) {
+    return ResponseEntity.ok()
+        .body(CommonResponse.<T>builder()
+            .statusCode(HttpStatus.OK.value())
+            .msg(msg)
+            .data(data)
+            .build());
+  }
 
 }
