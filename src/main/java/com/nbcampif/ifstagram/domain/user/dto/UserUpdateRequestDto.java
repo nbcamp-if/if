87f94f1d809401c0bbcfba0debb45d10b6cbe400
ruh0n <wns8256@naver.com>
2024-02-28 12:19:51 +0900

    ✨ feat(user): implement all-args constructor for testing

--- src/main/java/com/nbcampif/ifstagram/domain/user/dto/UserUpdateRequestDto.java
+++ src/main/java/com/nbcampif/ifstagram/domain/user/dto/UserUpdateRequestDto.java
@@ -1,14 +1,15 @@
 package com.nbcampif.ifstagram.domain.user.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserUpdateRequestDto implements Serializable {
 
   private String introduction;
